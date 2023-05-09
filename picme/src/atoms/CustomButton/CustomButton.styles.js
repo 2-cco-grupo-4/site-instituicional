208f50e6506e1c9dff27d353a3ac4668cb51166d
ryan-miyazato <ryan.miyazato@sptech.school>
2023-05-08 21:02:33 -0300

    fix(CustomButton): define button size

--- picme/src/atoms/CustomButton/CustomButton.styles.js
+++ picme/src/atoms/CustomButton/CustomButton.styles.js
@@ -3,13 +3,14 @@ const makeStyles = () => {
     {
       btn: {
         fontFamily: 'Inter, sans-serif',
+        fontSize: 16,
         textTransform: 'none',
         paddingRight: 2,
         paddingLeft: 2,
         paddingTop: 0,
         paddingBottom: 0,        
-        width: 'auto', 
-        height: 'auto',
+        width: 'fit-content', 
+        height: 32,
         whiteSpace: 'nowrap',
         borderRadius: 0.5,
       },
