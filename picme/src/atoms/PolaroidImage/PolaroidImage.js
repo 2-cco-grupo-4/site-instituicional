88b2d6a30343091151cec9a0eaacb79acb6f7b88
ryan-miyazato <ryan.miyazato@sptech.school>
2023-05-16 21:41:47 -0300

    feat(PolaroidImage): add polaroid image component

--- picme/src/atoms/PolaroidImage/PolaroidImage.js
+++ picme/src/atoms/PolaroidImage/PolaroidImage.js
@@ -1,16 +1,38 @@
 import useStyles from "./PolaroidImage.styles"
 
 import tape from 'assets/img/tape.png'
+import { Box } from "@mui/material"
+import { useEffect, useState } from "react"
 
-const PolaroidImage = () => {
+const PolaroidImage = ({children, tilt, hasTapes, ...props}) => {
   const classes = useStyles()
+  const [tiltDeg, setTiltDeg] = useState(0)
+
+  useEffect(() => {
+    setTiltDeg(tilt === 'left' ? -2 : 2) 
+  },[])
 
   return (
-    <>
-      <img className={imgTapeLeft} src={tape} />
-      <img className={imgTapeRight} src={tape} />
-      <img class="img-promocional img-promocional-1" src="assets/img/usuario-smartphone.jpg" />    
-    </>
+    <Box 
+    position="relative" 
+    alignItems="center" 
+    width="100%"
+    zIndex={1} 
+    sx={{
+      transform: `rotate(${tiltDeg}deg)`,
+    }}
+    {...props}
+    >
+      {hasTapes && (
+        <Box className={classes.tapes}>
+          <img src={tape} alt="" />
+          <img src={tape} alt="" />
+        </Box>
+      )}
+      <Box className={classes.polaroid}>
+        {children}
+      </Box>
+    </Box>
   )
 }
 
--- picme/src/atoms/PolaroidImage/PolaroidImage.styles.js
+++ picme/src/atoms/PolaroidImage/PolaroidImage.styles.js
@@ -1,8 +1,35 @@
 import { makeStyles } from '@mui/styles'
 
-export default makeStyles(() => ({
-  teste: {
-    backgroundColor: '#000',
-    color: '#fff',
+export default makeStyles((theme) => ({
+  tapes: {
+    position: 'absolute',
+    zIndex: '10',
+    top: '-5%',
+    width: '100%',
+    display: 'flex',
+    justifyContent: "space-between",
+    '& img': {
+      position: 'absolute',
+      width: '25%',
+      '&:first-child': {
+        transform: 'rotate(90deg)',
+        left: '-6%',
+      },
+      '&:last-child': {
+        right: '-6%',
+      }
+    }
   },
+  polaroid: {
+    padding: theme.spacing(2, 2, 6, 2),
+    backgroundColor: "#F2F2F2",
+    boxShadow: '0px 2px 8px rgba(0,0,0,.2)',
+    '& img': {
+      boxShadow: 'inset 0px 8px 8px rgba(0,0,0,.2)',
+      filter: 'contrast(130%)',
+      objectFit: 'cover',
+      width: '100%',
+      height: 400,
+    }
+  }
 }))
