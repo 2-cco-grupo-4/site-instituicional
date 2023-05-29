6cd669f8545759f1f99c20712d4fff837d552d88
ryan-miyazato <ryan.miyzato@sptech.school>
2023-05-29 18:54:05 -0300

    feat(Header): add routes to header

--- src/molecules/Header/Header.js
+++ src/molecules/Header/Header.js
@@ -3,7 +3,7 @@ import logo from "assets/img/picme.png"
 import OptionNav from "atoms/OptionNav"
 import CustomButton from "atoms/CustomButton/CustomButton"
 import { Box, Typography } from "@mui/material"
-import { HEADER_HEIGHT } from "utils/constants"
+import { HEADER_HEIGHT, ROUTES } from "utils/constants"
 import Container from "atoms/Container"
 import { useUserContext } from "contexts"
 import { useNavigate } from "react-router-dom"
@@ -45,13 +45,28 @@ const Header = ({ type }) => {
          ) : (
           type === 2 ? (
             <Box className={classes.navBotoes}>
-             <Typography sx={{ marginRight: 2 }}>Já possui conta?</Typography>
-             <CustomButton type="button" onClick={() => handleNavigation("/login")} variant="outlined" sx={{ marginRight: 2 }} color="secondary">Login</CustomButton>
+              <Typography sx={{ marginRight: 2 }}>Já possui conta?</Typography>
+              <CustomButton type="button" onClick={() => handleNavigation(ROUTES.LOGIN)} variant="outlined"color="secondary">
+                Login
+              </CustomButton>
            </Box>
           ) : (
             <Box className={classes.navBotoes}>
-                <CustomButton type="button" onClick={() => handleNavigation("/login")} variant="outlined" sx={{ marginRight: 2 }} color="secondary">Login</CustomButton>
-                <CustomButton type="button" onClick={() => handleNavigation("/persona")} variant="contained" color="primary">
+                <CustomButton 
+                type="button" 
+                onClick={() => handleNavigation(ROUTES.LOGIN)} 
+                variant="outlined" 
+                sx={{ marginRight: 2 }} 
+                color="secondary"
+                >
+                  Login
+                </CustomButton>
+                <CustomButton 
+                type="button" 
+                onClick={() => handleNavigation(ROUTES.CHOOSE_PROFILE)} 
+                variant="contained" 
+                color="primary"
+                >
                   Cadastro
                 </CustomButton>
             </Box>
