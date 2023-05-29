da8b089e9ea4b599a44e78b38ddccdc14ee6c13e
ryan-miyazato <ryan.miyzato@sptech.school>
2023-05-29 19:01:10 -0300

    feat(ChooseProfileType): add profile types

--- src/pages/ChooseProfileType/ChooseProfileType.js
+++ src/pages/ChooseProfileType/ChooseProfileType.js
@@ -1,7 +1,11 @@
 import useStyles from "./ChooseProfileType.styles"
 import Header from 'molecules/Header'
-import { Stack } from "@mui/material"
-import { Container } from "@mui/system"
+import { Stack, Typography } from "@mui/material"
+import DashedCard from "atoms/DashedCard"
+import Container from "atoms/Container"
+import imageCustomer from "assets/img/navigating-through-feed.webp"
+import imagePhotographer from "assets/img/photographer-nature.jpg"
+import CustomButton from "atoms/CustomButton"
 
 const ChooseProfileType = () => {
   const classes = useStyles()
@@ -9,10 +13,35 @@ const ChooseProfileType = () => {
   return (
     <>
       <Header type={2} />
-      <Stack>
-        <Container>
-        </Container>
-      </Stack>
+      <Container>
+        <Stack className={classes.content} spacing={4}>
+          <Typography variant="title-small-bold">Em qual perfil você se encaixa?</Typography>
+          <Stack direction="row" spacing={5}>
+            <DashedCard width={350} alignItems="center" spacing={4}>
+              <img src={imageCustomer} alt="customer-navigating-through-feed" className={classes.image}/>
+              <Typography variant="paragraph-medium-regular" align="center" width={280}>
+               Quero contratar fotógrafos e registrar momentos inesquecíveis
+              </Typography>
+              <CustomButton variant="contained" color="secondary"fullWidth>
+                <Typography variant="paragraph-small-regular" color="white">
+                  Cadastrar como <strong>cliente</strong>
+                </Typography>
+              </CustomButton>
+            </DashedCard>
+            <DashedCard width={350} alignItems="center" spacing={4}>
+              <img src={imagePhotographer} alt="customer-navigating-through-feed" className={classes.image}/>
+              <Typography variant="paragraph-medium-regular" align="center" width={280}>
+                Quero mostrar meu trabalho para o mundo
+              </Typography>
+              <CustomButton variant="contained" color="secondary"fullWidth>
+                <Typography variant="paragraph-small-regular" color="white">
+                  Cadastrar como <strong>fotógrafo</strong>
+                </Typography>
+              </CustomButton>
+            </DashedCard>
+          </Stack>
+        </Stack>
+      </Container>
     </>
   )
 }
--- src/pages/ChooseProfileType/ChooseProfileType.styles.js
+++ src/pages/ChooseProfileType/ChooseProfileType.styles.js
@@ -1,8 +1,16 @@
 import { makeStyles } from '@mui/styles'
 
-export default makeStyles(() => ({
-  teste: {
-    backgroundColor: '#000',
-    color: '#fff',
+export default makeStyles((theme) => ({
+  content: {
+    width: "100%",
+    padding: theme.spacing(4,0),
+    alignItems: "center", 
+    justifyContent: "top",
+  },
+  image: {
+    height: 200,
+    width: "100%",
+    objectFit: "cover",
+    borderRadius: theme.shape.borderRadius,
   },
 }))
