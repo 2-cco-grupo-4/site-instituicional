66b2b039e03b12fc789b5347dd5e57b4326cc8e7
ryan-miyazato <ryan.miyzato@sptech.school>
2023-05-29 21:56:04 -0300

    feat(ChooseProfileType): add params to navigate

--- src/pages/ChooseProfileType/ChooseProfileType.js
+++ src/pages/ChooseProfileType/ChooseProfileType.js
@@ -6,9 +6,16 @@ import Container from "atoms/Container"
 import imageCustomer from "assets/img/navigating-through-feed.webp"
 import imagePhotographer from "assets/img/photographer-nature.jpg"
 import CustomButton from "atoms/CustomButton"
+import { useNavigate } from "react-router-dom"
+import { ROUTES } from "utils/constants"
 
 const ChooseProfileType = () => {
   const classes = useStyles()
+  const navigate = useNavigate()
+
+  const handleNavigation = (route) => {
+    navigate(route)
+  }
 
   return (
     <>
@@ -22,7 +29,7 @@ const ChooseProfileType = () => {
               <Typography variant="paragraph-medium-regular" align="center" width={280}>
                Quero contratar fotógrafos e registrar momentos inesquecíveis
               </Typography>
-              <CustomButton variant="contained" color="secondary"fullWidth>
+              <CustomButton onClick={() => handleNavigation(ROUTES.REGISTER + "/cliente")} variant="contained" color="secondary"fullWidth>
                 <Typography variant="paragraph-small-regular" color="white">
                   Cadastrar como <strong>cliente</strong>
                 </Typography>
@@ -33,7 +40,7 @@ const ChooseProfileType = () => {
               <Typography variant="paragraph-medium-regular" align="center" width={280}>
                 Quero mostrar meu trabalho para o mundo
               </Typography>
-              <CustomButton variant="contained" color="secondary"fullWidth>
+              <CustomButton onClick={() => handleNavigation(ROUTES.REGISTER + "/fotografo")}  variant="contained" color="secondary"fullWidth>
                 <Typography variant="paragraph-small-regular" color="white">
                   Cadastrar como <strong>fotógrafo</strong>
                 </Typography>
