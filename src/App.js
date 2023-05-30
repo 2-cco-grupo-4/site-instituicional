291a5831b77679747763c90ac8508477b380e864
ryan-miyazato <ryan.miyzato@sptech.school>
2023-05-29 21:53:17 -0300

    feat(App): add localization providers and register route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import Theme from "templates/Theme/Theme";
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { UserProvider } from "contexts";
 import { BrowserRouter, Route } from "react-router-dom";
-import Home from "pages/Home";
-import ChooseProfileType from "pages/ChooseProfileType";
 import { Routes } from "react-router-dom";
 
+import Home from "pages/Home";
+import ChooseProfileType from "pages/ChooseProfileType";
+import Register from "pages/Register";
+
 const DefaultProviders = ({children}) => (
-  <UserProvider>
-    {children}
-  </UserProvider>
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <UserProvider>
+      {children}
+    </UserProvider>
+  </LocalizationProvider>
 )
 
 function App() {
@@ -19,6 +25,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home />}/>
             <Route exact path="/persona" element={<ChooseProfileType />} />
+            <Route exact path="/cadastro/:profileType" element={<Register/>} />
           </Routes>
         </BrowserRouter>
       </DefaultProviders>
