d09937558360c58f4eda3bd649551255f17aa419
ryan-miyazato <ryan.miyazato@sptech.school>
2023-05-30 21:30:52 -0300

    feat(Register): add input masks and navigation

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,30 +1,78 @@
 import useStyles from "./Register.styles"
 import Container from "atoms/Container"
-import { FormControl, FormControlLabel, Grid, Radio, RadioGroup, Stack, TextField, Typography } from "@mui/material"
+import { FormControlLabel, Stack, Radio, RadioGroup, TextField, Typography, Grid, IconButton, SvgIcon, Button } from "@mui/material"
 import LogoPicme from "atoms/LogoPicme"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { DatePicker } from "@mui/x-date-pickers"
+import CustomButton from "atoms/CustomButton"
+import InputMask from "react-input-mask"
+import { useState } from "react"
+import goBackArrow from "assets/icons/go-back-arrow.svg"
+import { ROUTES } from "utils/constants"
 
 const Register = () => {
   const classes = useStyles()
+  const navigate = useNavigate()
   const { profileType } = useParams()
+  const [profile, setProfile] = useState(profileType)
+  const [name, setName] = useState('')
+  const [surname, setSurname] = useState('')
+  const [email, setEmail] = useState('')
+  const [birthdate, setBirthdate] = useState('')
+  const [cpf, setCpf] = useState(null)
+  const [phone, setPhone] = useState(null)
+  const [password, setPassword] = useState('')
+
+  const CONTENTS = [
+    <TextField id="email-ipt" label="E-mail" />,
+  
+    <DatePicker id="data-ipt" label="Data de nascimento" format="DD/MM/YYYY"/>,
+  
+    <InputMask mask="999.999.999-99">
+      {() => <TextField id="cpf-ipt" label="CPF"/>}
+    </InputMask>,
+  
+    <InputMask mask="(99) 99999-9999">
+      {() => <TextField id="phone-ipt" label="Telefone"/>}
+    </InputMask>,
+  
+    <TextField id="password-ipt" type="password" label="Senha" />,
+  
+    <TextField id="verify-password-ipt" type="password" label="Confirmar Senha" />
+  ]
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    // fazer validações
+  }
 
   return (
-    <Grid container>
-      <Grid item className={classes.banner} xs={14} sm={7}>
-        <Container>
-          
+    <Stack direction="row" alignItems="top">
+      <Stack className={classes.banner}>
+        <Container py={4}>
+          <img 
+          src={goBackArrow} 
+          alt="go-back-arrow" 
+          style={{cursor: "pointer"}}
+          onClick={() => navigate(ROUTES.HOME)}
+          />
         </Container>
-      </Grid>
-      <Grid item className={classes.content} xs={10} sm={5}>
-        <Container>
-          <FormControl fullWidth>
-            <Stack width="100%" alignItems="center" py={4} spacing={3}>
+      </Stack>
+      <Stack className={classes.content}>
+        <Container py={4}>
+          <form onSubmit={handleSubmit}>
+            <Stack width="100%" minHeight="100%" alignItems="center" spacing={3}>
               <LogoPicme />
               <Typography variant="subtitle-small-semibold">Cadastre-se</Typography>
               <Stack direction="row" alignItems="center" spacing={2}>
                 <Typography variant="paragraph-medium-light">Sou um:</Typography>
-                <RadioGroup row aria-labelledby="radio-profile-type" defaultValue={profileType}>
+                <RadioGroup 
+                row 
+                aria-labelledby="radio-profile-type" 
+                defaultValue={profileType} 
+                onChange={(e) => setProfile(e.target.value)}
+                >
                   <FormControlLabel 
                   value="cliente" 
                   control={<Radio />} 
@@ -33,24 +81,41 @@ const Register = () => {
                   <FormControlLabel 
                   value="fotografo" 
                   control={<Radio />}  
-                  label={<Typography variant="paragraph-medium-light">Fotógrafo</Typography>} />
+                  label={<Typography variant="paragraph-medium-light">Fotógrafo</Typography>} 
+                  />
+
                 </RadioGroup>
               </Stack>
-              <Stack direction="row" alignItems="top">
-                <Stack spacing={2}>
-                  <TextField id="name-ipt" required label="Nome" />
-                  <DatePicker label="Data de nascimento" format="DD/MM/YYYY"/>
-                  <TextField id="name-ipt" required label="Nome" />
-                </Stack>
-                <Stack spacing={2}>
-
-                </Stack>
+              <Stack justifyContent="center">
+                <TextField id="name-ipt" label="Nome" sx={{marginBottom: 2}} width="100%" onChange={(e) => setName(e.target.value)}/>
+                <Grid container rowSpacing={2} columnSpacing={{ xs: 2 }}>
+                  {
+                    CONTENTS.map((content, index) => (
+                      <Grid item xs={6} key={index}>
+                        {content}
+                      </Grid>
+                    ))
+                  }
+                </Grid>
+              </Stack>
+              <CustomButton variant="contained" color="secondary" type="submit" fullWidth>Continuar</CustomButton>
+              <Stack direction="row" alignItems="center" spacing={0.5}>
+                <Typography sx={{fontWeight: 300}}>
+                  Já tem cadastro? 
+                </Typography>
+                <Typography 
+                color="primary" 
+                onClick={() => navigate(ROUTES.LOGIN)}
+                sx={{fontWeight: "bold", cursor: "pointer"}}
+                >
+                  Fazer login
+                </Typography>
               </Stack>
             </Stack>
-          </FormControl>
+          </form>
         </Container>
-      </Grid>
-    </Grid>
+      </Stack>
+    </Stack>
   )
 }
 
