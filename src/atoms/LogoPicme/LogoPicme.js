c8ccdf84029608a6ca0f27d9dd88286d8b64bdfa
ryan-miyazato <ryan.miyazato@sptech.school>
2023-06-06 17:44:56 -0300

    feat(LogoPicme): add height prop

--- src/atoms/LogoPicme/LogoPicme.js
+++ src/atoms/LogoPicme/LogoPicme.js
@@ -1,21 +1,23 @@
 import useStyles from "./LogoPicme.styles"
 import logo from "assets/img/picme.png"
 import { useNavigate } from "react-router-dom"
+import { ROUTES } from "utils/constants"
 
-const LogoPicme = ({ route, ...props }) => {
+const LogoPicme = ({height, ...props }) => {
   const classes = useStyles()
   const navigate = useNavigate()
 
-  const handleNavigation = () => {
+  const handleNavigation = (route) => {
     navigate(route)
   }
 
   return (
     <img 
     src={logo} 
-    onClick={() => handleNavigation(route)} 
+    onClick={() => handleNavigation(ROUTES.HOME)} 
     className={classes.logo} 
     alt="logo" 
+    height={height ?? 24}
     {...props} 
     />
   )
--- src/atoms/LogoPicme/LogoPicme.styles.js
+++ src/atoms/LogoPicme/LogoPicme.styles.js
@@ -2,7 +2,6 @@ import { makeStyles } from "@mui/styles"
 
 export default makeStyles(() => ({
   logo: {
-    height: 24,
     width: 'auto',
     padding: '8px 0px',
     cursor: 'pointer',
