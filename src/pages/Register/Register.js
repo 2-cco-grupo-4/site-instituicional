8c56f9e34343a992851319b3a29f87f0460e2272
ryan-miyazato <ryan.miyazato@sptech.school>
2023-06-06 07:44:30 -0300

    feat(Register): add form validation and errors

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,6 +1,6 @@
 import useStyles from "./Register.styles"
 import Container from "atoms/Container"
-import { FormControlLabel, Stack, Radio, RadioGroup, TextField, Typography, Grid } from "@mui/material"
+import { FormControlLabel, Stack, Radio, RadioGroup, TextField, Typography } from "@mui/material"
 import LogoPicme from "atoms/LogoPicme"
 import { useNavigate, useParams } from "react-router-dom"
 import CustomButton from "atoms/CustomButton"
@@ -8,8 +8,12 @@ import InputMask from "react-input-mask"
 import { useState } from "react"
 import goBackArrow from "assets/icons/go-back-arrow.svg"
 import { ROUTES } from "utils/constants"
-import { toTitleCase } from "utils/string"
+import { toTitleCase } from "utils/helpers/string"
 import { userDataSchema } from "./Register.schema"
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { useTheme } from "@mui/styles"
+import { useAsyncState } from "hooks/useAsyncState"
 
 const defaultValue = {
   nome: '',
@@ -21,70 +25,40 @@ const defaultValue = {
 }
 
 const Register = () => {
+  const theme = useTheme()
   const classes = useStyles()
   const navigate = useNavigate()
   const { profileType } = useParams()
 
   const [profile, setProfile] = useState(profileType)
-  const [userData, setUserData] = useState(defaultValue)
-  const [confirmarSenha, setConfirmarSenha] = useState('')
+  const [userData, setUserData, getState] = useAsyncState(defaultValue)
+  const [btnLoading, setBtnLoading] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({resolver: yupResolver(userDataSchema)})
 
   const handleChange = (e) => {
-    const name = e.target.name
-
-    if(name === "nome"){
+    if(e.target.name === "nome"){
       e.target.value = toTitleCase(e.target.value)
     }
-
-    const value = e.target.value
-
-    console.log(name, value)
-    setUserData(values => ({...values, [name]: value}))
   }
 
   const handleProfileChange = (e) => {
     setProfile(e.target.value)
   }
 
-  const CONTENTS = [
-    <TextField id="email-ipt" name="email" label="E-mail" onChange={handleChange}/>,
-  
-    <InputMask 
-    onChange={handleChange} 
-    mask="99/99/9999"
-    >
-      {() => <TextField id="data-ipt" name="dataNasc"
-    label="Data de nascimento" placeholder="DD/MM/AAAA" />}
-    </InputMask>,
-  
-    <InputMask mask="999.999.999-99" onChange={handleChange}>
-      {() => <TextField id="cpf-ipt" name="cpf" label="CPF" />}
-    </InputMask>,
-  
-    <InputMask mask="(99) 99999-9999" onChange={handleChange}>
-      {() => <TextField id="phone-ipt" name="numCelular" label="Telefone"/>}
-    </InputMask>,
-  
-    <TextField id="password-ipt" name="senha" type="password" label="Senha" onChange={handleChange}/>,
-  
-    <TextField 
-    id="verify-password-ipt" 
-    name="confirmarSenha" 
-    type="password" 
-    label="Confirmar Senha" 
-    onChange={(e) => setConfirmarSenha(e.target.value)}
-    />
-  ]  
+  const onSubmitHandler = async (data) => {
+    setBtnLoading(true)
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setUserData(current => ({...current, dataNasc: userData["dataNasc"].replace(/\//g,"-")}))
-    userDataSchema.isValid(userData).then((valid) => {
-      if(valid) {
-        console.log(userData)
-      }
-    })
+    const payload = {...data, dataNasc: data["dataNasc"].replace(/\//g,"-")}
+    delete payload["confirmarSenha"]
+    setUserData(payload)
+    console.log(await getState())
   }
+
 
   return (
     <Stack direction="row" alignItems="top">
@@ -99,71 +73,125 @@ const Register = () => {
         </Container>
       </Stack>
       <Stack className={classes.content}>
-        <Container py={4}>
-          <form onSubmit={handleSubmit}>
-            <Stack width="100%" minHeight="100%" alignItems="center" spacing={3}>
-              <LogoPicme />
-              <Typography variant="subtitle-small-semibold">Cadastre-se</Typography>
-              <Stack direction="row" alignItems="center" spacing={2}>
-                <Typography variant="paragraph-medium-light">Sou um:</Typography>
-                <RadioGroup 
-                row 
-                aria-labelledby="radio-profile-type" 
-                name="profile"
-                onChange={handleProfileChange}
-                defaultValue={profileType} 
-                >
-                  <FormControlLabel 
-                  value="cliente" 
-                  control={<Radio />} 
-                  label={<Typography variant="paragraph-medium-light">Cliente</Typography>} />
+        <Container component="form" onSubmit={handleSubmit(onSubmitHandler)} py={4}>
+          <Stack width="100%" minHeight="100%" alignItems="center" spacing={3}>
+            <LogoPicme />
+            <Typography variant="subtitle-small-semibold">Cadastre-se</Typography>
+            <Stack direction="row" alignItems="center" spacing={2}>
+              <Typography variant="paragraph-medium-light">Sou um:</Typography>
+              <RadioGroup 
+              row 
+              aria-labelledby="radio-profile-type" 
+              name="profile"
+              onChange={handleProfileChange}
+              defaultValue={profileType} 
+              >
+                <FormControlLabel 
+                value="cliente" 
+                control={<Radio />} 
+                label={<Typography variant="paragraph-medium-light">Cliente</Typography>} />
 
-                  <FormControlLabel 
-                  value="fotografo" 
-                  control={<Radio />}  
-                  label={<Typography variant="paragraph-medium-light">Fotógrafo</Typography>} 
+                <FormControlLabel 
+                value="fotografo" 
+                control={<Radio />}  
+                label={<Typography variant="paragraph-medium-light">Fotógrafo</Typography>} 
+                />
+
+              </RadioGroup>
+            </Stack>
+            <Stack justifyContent="top" width="100%" spacing={2} sx={{
+              '& .MuiFormHelperText-root': {
+                fontSize: theme.typography['paragraph-xsmall-regular'].fontSize,
+              }
+            }}>
+              <TextField 
+              id="name-ipt"
+              name="nome"
+              {...register("nome")}
+              error={!!errors.nome}
+              helperText={errors.nome?.message}
+              label="Nome" 
+              width="100%" 
+              onChange={handleChange}
+              />
+              <Stack spacing={3}>
+                <Stack direction="row" width="100%" alignItems="top" spacing={2}>
+                  <TextField 
+                  id="email-ipt" 
+                  name="email" 
+                  label="E-mail" 
+                  {...register("email")}
+                  onChange={handleChange}
+                  error={!!errors.email}
+                  helperText={errors.email?.message}
+                  fullWidth
                   />
 
-                </RadioGroup>
-              </Stack>
-              <Stack justifyContent="center">
-                <TextField 
-                id="name-ipt"
-                inputProps={{
-                  name: "nome"
-                }}
-                label="Nome" 
-                sx={{marginBottom: 2}} 
-                width="100%" 
-                onChange={handleChange}
-                />
-                <Grid container rowSpacing={2} columnSpacing={{ xs: 2 }}>
-                  {
-                    CONTENTS.map((content, index) => (
-                      <Grid item xs={6} key={index}>
-                        {content}
-                      </Grid>
-                    ))
-                  }
-                </Grid>
-              </Stack>
-              <CustomButton variant="contained" color="secondary" type="submit" fullWidth>
-                Continuar
-              </CustomButton>
-              <Stack direction="row" alignItems="center" spacing={0.5}>
-                <Typography sx={{fontWeight: 300}}>
-                  Já tem cadastro? 
-                </Typography>
-                <Typography 
-                color="primary" 
-                onClick={() => navigate(ROUTES.LOGIN)}
-                sx={{fontWeight: "bold", cursor: "pointer"}}
-                >
-                  Fazer login
-                </Typography>
+                  <InputMask 
+                  {...register("dataNasc")}
+                  onChange={handleChange} 
+                  mask="99/99/9999"
+                  >
+                    {() => <TextField id="data-ipt" name="dataNasc" fullWidth
+                    label="Data de nascimento" placeholder="DD/MM/AAAA" 
+                    error={!!errors.dataNasc} helperText={errors.dataNasc?.message}
+                    />}
+                  </InputMask>
+                </Stack>
+                <Stack direction="row" width="100%" alignItems="top" spacing={2}>
+                  <InputMask mask="999.999.999-99" {...register("cpf")} onChange={handleChange} >
+                    {() => <TextField id="cpf-ipt" name="cpf" label="CPF" fullWidth error={!!errors.cpf}
+                    helperText={errors.cpf?.message} />} 
+                  </InputMask>
+                  
+                  <InputMask mask="(99) 99999-9999" {...register("numCelular")} onChange={handleChange}>
+                  {() => <TextField id="phone-ipt" name="numCelular" label="Telefone" fullWidth
+                  error={!!errors.numCelular} helperText={errors.numCelular?.message}/>}
+                  </InputMask>
+                </Stack>
+                <Stack direction="row" width="100%" alignItems="top" spacing={2}>
+                  <TextField 
+                  id="password-ipt" 
+                  name="senha" 
+                  type="password" 
+                  label="Senha" 
+                  fullWidth
+                  {...register("senha")}
+                  onChange={handleChange}
+                  error={!!errors.senha}
+                  helperText={errors.senha?.message}
+                  />
+
+                  <TextField 
+                  id="verify-password-ipt" 
+                  name="confirmarSenha" 
+                  type="password" 
+                  label="Confirmar Senha" 
+                  fullWidth
+                  {...register("confirmarSenha")}
+                  onChange={handleChange}
+                  helperText={errors.confirmarSenha?.message}
+                  error={!!errors.confirmarSenha}
+                  />
+                </Stack>
               </Stack>
             </Stack>
-          </form>
+            <CustomButton loading={btnLoading} variant="contained" color="secondary" type="submit" fullWidth>
+              Continuar
+            </CustomButton>
+            <Stack direction="row" alignItems="center" spacing={0.5}>
+              <Typography sx={{fontWeight: 300}}>
+                Já tem cadastro? 
+              </Typography>
+              <Typography 
+              color="primary" 
+              onClick={() => navigate(ROUTES.LOGIN)}
+              sx={{fontWeight: "bold", cursor: "pointer"}}
+              >
+                Fazer login
+              </Typography>
+            </Stack>
+          </Stack>
         </Container>
       </Stack>
     </Stack>
--- src/pages/Register/Register.schema.js
+++ src/pages/Register/Register.schema.js
@@ -1,12 +1,13 @@
 import * as yup from 'yup'
 
 const userDataSchema = yup.object({
-  nome: yup.string().required("Campo obrigatório"),
+  nome: yup.string().required("Campo obrigatório").min(1, "Nome inválido").max(32, "Máximo 32 caractéres"),
   email: yup.string().email("E-mail inválido").required("Campo obrigatório"),
   dataNasc: yup.string().required("Campo obrigatório"),
   cpf: yup.string().required("Campo obrigatório"),
   numCelular: yup.string().required("Campo obrigatório"),
-  senha: yup.string().required("Campo obrigatório")
+  senha: yup.string().required("Campo obrigatório").min(8,"Mínimo de 8 caractéres").max(32, "Máximo de 32 caractéres"),
+  confirmarSenha: yup.string().oneOf([yup.ref('senha'), null], 'Senhas não coincidem').required("Campo obrigatório")
 }).required()
 
 export { userDataSchema }
