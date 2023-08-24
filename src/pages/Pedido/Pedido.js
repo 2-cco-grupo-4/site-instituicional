38d7f40924d28132d06efeff406831526994ab8a
eduardanv <maria.neves@sptech.school>
2023-08-24 16:15:05 -0300

    continuacao da tela pedido, table configurada

--- src/pages/Pedido/Pedido.js
+++ src/pages/Pedido/Pedido.js
@@ -8,69 +8,116 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Header from 'molecules/Header'
 import useStyles from "./Pedido.styles"
-import { Box, Stack, TextField, Typography, useTheme,Container } from "@mui/material"
+import { Box, Stack, TextField, Typography, useTheme, Container, Button, InputLabel, MenuItem, FormControl, Select    } from "@mui/material"
 
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(foto, nomeFotografo, status, evento, dataSolicitacao, detalhes) {
+  return { foto, nomeFotografo, status, evento, dataSolicitacao, detalhes };
 }
 
+function RepeatedButton({ label, onClick }) {
+  return (
+    <Button variant="contained" color="primary" onClick={onClick}>
+      {label}
+    </Button>
+  );
+}
+
+function BasicSelect() {
+  const [tema, setTema] = React.useState('');
+
+  const handleChange = (event) => {
+    setTema(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Tema</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={tema}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={tema}>Tema</MenuItem>
+          
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
+
+
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Foto', 'Teste Nome', 'Em espera', 'Casamento', '21/04/2023', 'Ver Detalhes', 'Teste'),
+  createData('Foto', 'Teste Nome', 'Negociando', 'Aniversário', '14/04/2023', 'Ver Detalhes'),
+  createData('Foto', 'Teste Nome', 'Agendado', 'Formatura', '26/02/2023', 'Ver Detalhes'),
+  createData('Foto', 'Teste Nome', 'Concluído', 'Debutante', '06/11/2022', 'Ver Detalhes'),
+  createData('Foto', 'Teste Nome', 'Concluído', 'Chá de Bebê', '12/10/2022', 'Ver Detalhes'),
 ];
 
 
 
 const head = () => {
-  
+
   return (
     <Stack sx={{ transition: '2s all ease' }}>
-      
+
       <Header type={1} />
       <Container
         alignItems="center"
-      
+
       >
       </Container>
-      </Stack>
+    </Stack>
   )
 }
 
+
 export default function BasicTable() {
   return (
-    
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+    <Box>
+      <Typography variant="subtitle-small-semibold" sx={{ marginBottom: '50px', fontSize: '25px' }}>
+        Pedidos
+      </Typography>
+      <Typography sx={{ marginTop: '25px', marginBottom: '25px', fontSize: '18px' }}>
+        Filtrar:
+      </Typography>
+      <BasicSelect />
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Foto</TableCell>
+              <TableCell align="center">Nome do Fotógrafo</TableCell>
+              <TableCell align="center">Status</TableCell>
+              <TableCell align="center">Evento</TableCell>
+              <TableCell align="center">Data de Solicitação</TableCell>
+              <TableCell align="center">Detalhes</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.foto}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell align="center" component="th" scope="row" >
+                  {row.foto}
+                </TableCell>
+                <TableCell align="center">{row.nomeFotografo}</TableCell>
+                <TableCell align="center">{row.status}</TableCell>
+                <TableCell align="center">{row.evento}</TableCell>
+                <TableCell align="center">{row.dataSolicitacao}</TableCell>
+                <TableCell align="center"><Button onClick={RepeatedButton}>Ver Detalhes</Button></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
