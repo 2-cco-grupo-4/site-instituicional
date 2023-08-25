58d707ba0396780d9009757d07abdd6ed02caa0f
eduardanv <maria.neves@sptech.school>
2023-08-24 21:34:09 -0300

    feat(Pedido): finished order page, just have to style it.

--- src/pages/Pedido/Pedido.js
+++ src/pages/Pedido/Pedido.js
@@ -8,11 +8,24 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Header from 'molecules/Header'
 import useStyles from "./Pedido.styles"
-import { Box, Stack, TextField, Typography, useTheme, Container, Button, InputLabel, MenuItem, FormControl, Select    } from "@mui/material"
-
+import { Box, Stack, TextField, Typography, useTheme, Container, Button, InputLabel, MenuItem, FormControl, Select, FormGroup } from "@mui/material"
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 function createData(foto, nomeFotografo, status, evento, dataSolicitacao, detalhes) {
   return { foto, nomeFotografo, status, evento, dataSolicitacao, detalhes };
+}
+
+function BasicDatePicker() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DatePicker']}>
+        <DatePicker label="Data" />
+      </DemoContainer>
+    </LocalizationProvider>
+  );
 }
 
 function RepeatedButton({ label, onClick }) {
@@ -24,27 +37,71 @@ function RepeatedButton({ label, onClick }) {
 }
 
 function BasicSelect() {
-  const [tema, setTema] = React.useState('');
+  const [theme, setTema] = React.useState('');
 
-  const handleChange = (event) => {
+  const handleChangeTheme = (event) => {
     setTema(event.target.value);
+  };
+
+  const [status, setStatus] = React.useState('');
+  const handleChangeStatus = (event) => {
+    setStatus(event.target.value);
+  };
+
+  const [dataSolicitacao, setDataSolicitacao] = React.useState('');
+  const handleChangeDataSolicitacao = (event) => {
+    setDataSolicitacao(event.target.value);
   };
 
   return (
     <Box sx={{ minWidth: 120 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Tema</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={tema}
-          label="Age"
-          onChange={handleChange}
-        >
-          <MenuItem value={tema}>Tema</MenuItem>
-          
-        </Select>
-      </FormControl>
+      <FormGroup>
+        <FormControl width="20%">
+          <InputLabel sx={{display:'flex'}} id="demo-simple-select-label">Tema</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={theme}
+            label="Theme"
+            onChange={handleChangeTheme}
+          >
+            <MenuItem value={theme}>Aniversário</MenuItem>
+            <MenuItem value={theme}>Casamento</MenuItem>
+            <MenuItem value={theme}>Chá de Bebê</MenuItem>
+            <MenuItem value={theme}>Debutante</MenuItem>
+            <MenuItem value={theme}>Formatura</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl width="300px">
+          <InputLabel id="demo-simple-select-label">Status</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={status}
+            label="Status"
+            onChange={handleChangeStatus}
+          >
+            <MenuItem value={status}>Agendado</MenuItem>
+            <MenuItem value={status}>Cancelado</MenuItem>
+            <MenuItem value={status}>Concluído</MenuItem>
+            <MenuItem value={status}>Em espera</MenuItem>
+            <MenuItem value={status}>Negociando</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label"></InputLabel>
+          <BasicDatePicker
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={dataSolicitacao}
+            onChange={handleChangeDataSolicitacao}
+          />
+
+        </FormControl>
+      </FormGroup>
+
     </Box>
   );
 }
@@ -78,46 +135,49 @@ const head = () => {
 
 export default function BasicTable() {
   return (
-    <Box>
-      <Typography variant="subtitle-small-semibold" sx={{ marginBottom: '50px', fontSize: '25px' }}>
-        Pedidos
-      </Typography>
-      <Typography sx={{ marginTop: '25px', marginBottom: '25px', fontSize: '18px' }}>
-        Filtrar:
-      </Typography>
-      <BasicSelect />
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">Foto</TableCell>
-              <TableCell align="center">Nome do Fotógrafo</TableCell>
-              <TableCell align="center">Status</TableCell>
-              <TableCell align="center">Evento</TableCell>
-              <TableCell align="center">Data de Solicitação</TableCell>
-              <TableCell align="center">Detalhes</TableCell>
-            </TableRow>
-          </TableHead>
-
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.foto}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell align="center" component="th" scope="row" >
-                  {row.foto}
-                </TableCell>
-                <TableCell align="center">{row.nomeFotografo}</TableCell>
-                <TableCell align="center">{row.status}</TableCell>
-                <TableCell align="center">{row.evento}</TableCell>
-                <TableCell align="center">{row.dataSolicitacao}</TableCell>
-                <TableCell align="center"><Button onClick={RepeatedButton}>Ver Detalhes</Button></TableCell>
+    <Stack sx={{ transition: '2s all ease' }}>
+      <Header type={2} />
+      <Box sx={{marginTop: '50px', p: 2}}>
+        <Typography variant="subtitle-small-semibold" sx={{marginBottom: '50px', fontSize: '25px' }}>
+          Pedidos
+        </Typography>
+        <Typography sx={{ marginTop: '25px', marginBottom: '25px', fontSize: '18px' }}>
+          Filtrar:
+        </Typography>
+        <BasicSelect />
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">Foto</TableCell>
+                <TableCell align="center">Nome do Fotógrafo</TableCell>
+                <TableCell align="center">Status</TableCell>
+                <TableCell align="center">Evento</TableCell>
+                <TableCell align="center">Data de Solicitação</TableCell>
+                <TableCell align="center">Detalhes</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
+            </TableHead>
+
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.foto}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell align="center" component="th" scope="row" >
+                    {row.foto}
+                  </TableCell>
+                  <TableCell align="center">{row.nomeFotografo}</TableCell>
+                  <TableCell align="center">{row.status}</TableCell>
+                  <TableCell align="center">{row.evento}</TableCell>
+                  <TableCell align="center">{row.dataSolicitacao}</TableCell>
+                  <TableCell align="center"><Button onClick={RepeatedButton}>Ver Detalhes</Button></TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Stack>
   );
 }
