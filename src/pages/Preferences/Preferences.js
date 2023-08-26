132c98a877cb2e48e0852b22f38710ce39a3fe3d
ryan-miyazato <ryan.miyazato@sptech.school>
2023-08-26 12:27:10 -0300

    feat(Preferences): add theme images

--- src/pages/Preferences/Preferences.js
+++ src/pages/Preferences/Preferences.js
@@ -1,15 +1,49 @@
 import Container from "atoms/Container/Container"
 import useStyles from "./Preferences.styles"
-import Header from 'molecules/Header'
-import { Stack, Typography, useTheme } from "@mui/material"
+import { Grid, Stack, Typography, useTheme } from "@mui/material"
 import LogoPicme from "atoms/LogoPicme/LogoPicme"
+import CustomButton from "atoms/CustomButton/CustomButton"
+
+import casamento from 'assets/img/tema-casamento.png'
+import esportivo from 'assets/img/tema-esportivo.png'
+import debutante from 'assets/img/tema-debutante.png'
+import familia from 'assets/img/tema-familia.png'
+import formatura from 'assets/img/tema-formatura.png'
+import aniversario from 'assets/img/tema-aniversario.png'
+
+const PREFERENCES_LIST = [
+  {
+    image: casamento,
+    name: "Casamento",
+  },
+  {
+    image: esportivo,
+    name: "Esportivo",
+  },
+  {
+    image: debutante,
+    name: "Debutante",
+  },
+  {
+    image: familia,
+    name: "Família",
+  },
+  {
+    image: formatura,
+    name: "Formatura",
+  },
+  {
+    image: aniversario,
+    name: "Aniversário",
+  },
+]
 
 const Preferences = () => {
   const theme = useTheme()
   const classes = useStyles()
 
   return (
-    <Container flexDirection="column" pt={4} rowGap={8}>
+    <Container flexDirection="column" py={4} rowGap={8}>
       <Stack flexDirection="column" rowGap={2}>
         <Stack flexDirection="row" width="100%" justifyContent="space-between">
           <Stack flexDirection="column">
@@ -17,15 +51,39 @@ const Preferences = () => {
               Quais são seus interesses?
             </Typography>
             <Typography>
-              Selecione os tópicos que você mais se identifica!
+              Selecione os tópicos com os quais você mais se identifica!
             </Typography>
           </Stack>
           <LogoPicme />
         </Stack>
-        <Stack>
+        <Stack flexDirection="row" justifyContent="space-between" alignItems="center">
           <Typography variant="subtitle-small-bold">Tema</Typography>
+          <CustomButton variant="contained" color="secondary">Continuar</CustomButton>
         </Stack>
       </Stack>
+      <Grid container spacing={2}>
+        {PREFERENCES_LIST.map((info, i) => (
+          <Grid key={info.name} item md={4}>
+            <Stack
+              className={classes.bg}
+              sx={{
+                backgroundImage: `url(${info.image})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            >
+              <Stack
+                className={classes.content}
+                sx={{
+                  background: 'linear-gradient(180deg, rgba(0, 0, 0, 0.00) 66.67%, #000 85.42%)',
+                }}
+              >
+                <Typography variant="paragraph-medium-bold">{info.name}</Typography>
+              </Stack>
+            </Stack>
+          </Grid>
+        ))}
+      </Grid>
     </Container>
   )
 }
--- src/pages/Preferences/Preferences.styles.js
+++ src/pages/Preferences/Preferences.styles.js
@@ -1,8 +1,20 @@
 import { makeStyles } from '@mui/styles'
 
-export default makeStyles(() => ({
-  teste: {
-    backgroundColor: '#000',
+export default makeStyles((theme) => ({
+  bg: {
     color: '#fff',
+    aspectRatio: 8 / 5,
+    borderRadius: theme.shape.borderRadius,
   },
+  content: {
+    width: '100%',
+    height: '100%',
+    padding: theme.spacing(2),
+    boxSizing: 'border-box',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    borderRadius: 'inherit',
+    cursor: 'pointer',
+  }
 }))
