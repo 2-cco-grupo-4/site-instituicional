24ad4fe71e4db4e09a00f6e26215caf3e0df7c19
DanyloSPTech <danylo.gomes@sptech.school>
2023-09-07 18:58:01 -0300

    Merge branch 'main' of https://github.com/2-cco-grupo-4/site-instituicional into feature/album

--- src/utils/helpers/string.js
+++ src/utils/helpers/string.js
@@ -6,3 +6,30 @@ export const toTitleCase = (str) => {
     }
   );
 }
+
+export const stringToColor = (string) => {
+  let hash = 0
+  let i
+
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash)
+  }
+
+  let color = '#'
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff
+    color += `00${value.toString(16)}`.slice(-2)
+  }
+
+  return color
+}
+
+export const stringAvatar = (name) => {
+  return ({
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name[0].toUpperCase()}`,
+  })
+}
