bf33a9ca654f432711596c59a1dbf8433a9be51d
DanyloSPTech <danylo.gomes@sptech.school>
2023-11-26 23:42:40 -0300

    fix perfil fotografo

--- src/service/fotografos.js
+++ src/service/fotografos.js
@@ -5,4 +5,8 @@ export const FOTOGRAFO = {
     api.get("/fotografos", {
       headers: { Authorization: `Bearer ${token}` },
     }),
+  BUSCAR_FOTOGRAFO: (idFotografo, token) =>
+    api.get(`/fotografos/buscar/id/${idFotografo}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    }),
 };
